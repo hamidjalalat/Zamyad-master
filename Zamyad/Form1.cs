a463d92c77cd5a88b2bbcdeed815046090fb5568
Moolana <jalalathamid@yahoo.com>
2020-10-16 13:40:11 +0330

    Add project files.

---
+++ Zamyad/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zamyad
+{
+
+    public delegate void XHandler(object sender, System.EventArgs e); //مرحله اول
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Stack oStack = new Stack();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            oStack.X += new XHandler(oStack_X); //مرحله پنجم
+        }
+
+        private static void oStack_X(object sender, System.EventArgs e) // مرحله ششم
+        {
+            System.Windows.Forms.MessageBox.Show("Stack Over Flow");
+        }
+
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Temp = Convert.ToInt32(textBox1.Text);
+            oStack.Amount = Temp + oStack.Amount;
+            textBox1.Text = "";
+        }
+    }
+}
---
+++ Zamyad/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zamyad
+{
+   public class Stack
+    {
+        public Stack()
+        {
+
+        }
+        static  private int _amount;
+		public int Amount
+		{
+			get
+			{
+				return (_amount);
+			}
+			set
+			{
+                if (value>=30)
+                {
+					onX(new System.EventArgs()); //مرحله چهارم
+				}
+				_amount = value;
+			}
+		}
+		public event XHandler X; //مرحله دوم
+		virtual protected void onX(System.EventArgs e) //مرحله سوم
+		{
+			if (X != null)
+				X(this, e);
+		}
+
+	
+	}
+}
